06197df39b58f89784eb15f45aba72c1c3303724
codewithdolly <hicoder.dolly@gmail.com>
2022-05-05 13:19:46 +0530

    css ans scss applied in multiple way

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import styles from "../styles/home.module.scss"
 
 const index = () => {
   return (
-    <div><center><h2>This is an API Page🎉</h2></center></div>
+    <div><center><h2>This is an <span className={styles.apiName}>API</span> <span className={styles.apipage}>Page🎉</span></h2></center>
+    
+    <center><p className={[styles.apiName, styles.apipage].join(" ")}> multiple css/scss APLY</p></center>
+    
+    </div>
   )
 }
 
